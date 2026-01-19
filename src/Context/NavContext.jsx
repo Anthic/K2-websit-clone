ebbbd54df16e17d942ee665857004e2a2db0aa59
Anthic Kumar Singh <anthickumarsingh2@gmail.com>
2026-01-19 23:09:29 +0600

    done all

--- src/Context/NavContext.jsx
+++ src/Context/NavContext.jsx
@@ -1,15 +1,29 @@
 /* eslint-disable react-refresh/only-export-components */
-import React, { createContext, useState } from 'react';
+import React, { createContext, useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
 
 export const NavbarContext = createContext()
-
+export const NavbarColorContext =createContext()
 
 function NavContext({children}) {
+    const [navColor,setNavColor] = useState("white")
     const [navOpen,setNavOpen]=useState(false)
+
+    const location =useLocation().pathname
+    useEffect(function(){
+        if (location == "/projects" || location == "/agence") {
+            setNavColor("black")
+        } else {
+            setNavColor("white")
+        }
+    },[location])
     return (
         <div>
             <NavbarContext.Provider value={[navOpen,setNavOpen]}>
-                {children}
+                <NavbarColorContext.Provider value={[navColor,setNavColor]}>
+                     {children}
+                </NavbarColorContext.Provider>
+               
             </NavbarContext.Provider>
         </div>
     );
--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -1,7 +1,90 @@
-import React from "react";
+import { useGSAP } from '@gsap/react';
+
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/all';
+import ProjectCard from '../components/Projects/ProjectCard';
 
 const Projects = () => {
-  return <div>this is projects</div>;
+  const projects = [
+    {
+      image1:
+        'https://k72.ca/uploads/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_1280x960-1280x960.jpg',
+      image2:
+        'https://k72.ca/uploads/caseStudies/WIDESCAPE/WS---K72.ca---Thumbnail-1280x960.jpg',
+    },
+    {
+      image1:
+        'https://k72.ca/uploads/caseStudies/OKA/OKA_thumbnail-1280x960.jpg',
+      image2:
+        'https://k72.ca/uploads/caseStudies/Opto/thumbnailimage_opto-1280x960.jpg',
+    },
+    {
+      image1:
+        'https://k72.ca/uploads/caseStudies/LAMAJEURE_-_Son_sur_mesure/chalaxeur-thumbnail_img-1280x960.jpg',
+      image2:
+        'https://k72.ca/uploads/caseStudies/SHELTON/thumbnailimage_shelton-1280x960.jpg',
+    },
+    {
+      image1:
+        'https://k72.ca/images/caseStudies/BEST/BEST_site_menu_Thumbnail.jpg?w=1280&h=960&s=d3b20d81946c6a7f4a6cd7ce1cfbb0fd',
+      image2:
+        'https://k72.ca/images/caseStudies/SollioAg/thumbnailimage_SollioAg.jpg?w=1280&h=960&s=3085861fabc3a15e7f8f8a01c07afa4f',
+    },
+    {
+      image1:
+        'https://k72.ca/images/caseStudies/OSM/thumbnail_OSM_1280.jpg?w=1280&h=960&s=968b0d5b6e3ac3b7ab84c1706efd4377',
+      image2:
+        'https://k72.ca/images/caseStudies/BAnQ_100TEMPS/100temps_Thumbnail.jpg?w=1280&h=960&s=5c944bb014f8643227ad7bb117fccc14',
+    },
+    {
+      image1:
+        'https://k72.ca/images/caseStudies/PME-MTL/PME-MTL_Thumbnail.jpg?w=1280&h=960&s=49e3b251d0a28f1f8d40fd59517fc000',
+      image2:
+        'https://k72.ca/images/caseStudies/FRUITE/Fruite_thumbnail_bbq.jpg?w=1280&h=960&s=953c1f702bec28d66d07e95bc1261821',
+    },
+  ];
+
+  gsap.registerPlugin(ScrollTrigger);
+
+  useGSAP(function () {
+    gsap.from('.hero', {
+      height: '100px',
+      opacity: 0, 
+      duration: 2, 
+      ease: 'power3.out', 
+      stagger: {
+        amount: .8,
+      },
+      scrollTrigger: {
+        trigger: '.lol',
+        start: 'top 90%',
+        end: 'top -120%',
+        scrub: true,
+      },
+    });
+  });
+
+  return (
+    <div className="lg:p-4 p-2 mb-[30vh]">
+      <div className=" pt-[45vh]">
+        <h2 className="font-[font2] lg:text-[9.5vw] text-7xl uppercase">
+          Projets
+        </h2>
+      </div>
+      <div className="-lg:mt-20 lol">
+        {projects.map(function (elem, idx) {
+          return (
+            <div
+              key={idx}
+              className="hero w-full lg:h-[400px] mb-4 flex lg:flex-row flex-col lg:gap-4 gap-2"
+            >
+              <ProjectCard image1={elem.image1} image2={elem.image2} />
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Projects;
